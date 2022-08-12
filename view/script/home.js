21359792bc14d45711e6a7e436da7ffa848d22dc
Peneva <dpeneva9@gmail.com>
2022-08-12 10:54:29 +0300

    #2pthr6b: frontend validation and error handling

--- view/script/home.js
+++ view/script/home.js
@@ -2,64 +2,83 @@ const email = document.getElementById('email');
 const roomId = document.getElementById('roomSprintId');
 const roomPass = document.getElementById('roomPassword');
 const button = document.getElementById('submit-btn');
-
 const error = document.getElementById('error-response');
+const form = document.getElementById('enter-room-form');
 
+const state = {
+    email: '',
+    roomId: '',
+    roomPass: ''
+};
 
-// const form = document.getElementById('enter-room-form');
-
-// const state = {
-//     epicLink: '',
-//     taskLink: '',
-//     taskDescription: '',
-//     tasks: []
-// };
-
-function initHome() {
+function init() {
     button.addEventListener('click', handleSubmit);
+    form.addEventListener('submit', handleSubmit);
+    email.addEventListener('keyup', (e) => handleFieldChange(e.target.value, 'email', email));
+    roomId.addEventListener('keyup', (e) => handleFieldChange(e.target.value, 'roomId', roomId));
+    roomPass.addEventListener('keyup', (e) => handleFieldChange(e.target.value, 'roomPass', roomPass));
 }
 
 function handleSubmit(e) {
     e.preventDefault();
-    console.log('>>> e', e);
 
-    if (!email.value) {
-        email.classList.add('invalid');
+    hideError();
+
+    if(!isFormValid()) {
+        showError('Fields cannot be empty');
+        return;
     }
-
-    if (!roomId.value) {
-        roomId.classList.add('invalid');
-    }
-
-    if (!roomPass.value) {
-        roomPass.classList.add('invalid');
-    }
-
-
-    // const data = new FormData(form);
     
-    // console.log('>>> form ', data);
-
-    const testt = {email: email.value, roomId: roomId.value, roomPass: roomPass.value};
-
+    const data = { ...state };
+    console.log('>> data', data);
     const ajaxReques = new XMLHttpRequest();
     ajaxReques.open('POST', 'service/EnterRoom.php');
-
-    ajaxReques.send(JSON.stringify(testt));
+    ajaxReques.send(JSON.stringify(data));
 
     ajaxReques.onreadystatechange = () => {
-        // console.log('>>> ajaxReques', ajaxReques);
-        if(ajaxReques.readyState === 4 && ajaxReques.status == 200) {
+        if (ajaxReques.readyState === 4 && ajaxReques.status == 200) {
             const response = JSON.parse(ajaxReques.responseText);
             
-            // if(response.)
-            console.log('>>> response', response);
-            
-            const textContent = document.createTextNode(ajaxReques.statusText);
-            error.appendChild(textContent);
-            error.style.display = "block";
+        } else if (ajaxReques.readyState === 4 && (ajaxReques.status === 400 || ajaxReques.status === 404)) {
+            const response = JSON.parse(ajaxReques.responseText);
+            showError(response.error);
         }
     }
 }
 
-document.addEventListener('DOMContentLoaded', initHome);
+const isFormValid = () => {
+    let flag = true;
+    if (isEmpty(email.value)) {
+        email.classList.add('invalid');
+        flag = false;
+    }
+
+    if (isEmpty(roomId.value)) {
+        roomId.classList.add('invalid');
+        flag = false;
+    }
+
+    if (isEmpty(roomPass.value)) {
+        roomPass.classList.add('invalid');
+        flag = false;
+    }
+    
+    return flag;
+}
+
+function handleFieldChange(newValue, field, element) {
+    state[field] = newValue;
+    hideError();
+    element.classList.remove('invalid');
+}
+
+const isEmpty = value => value && value.trim() !== '' ? false : true;
+
+const showError = (errorMessage) => { 
+    error.textContent = errorMessage;
+    error.classList.remove('hidden');
+}
+
+const hideError = () => error.classList.add('hidden');
+
+document.addEventListener('DOMContentLoaded', init);
